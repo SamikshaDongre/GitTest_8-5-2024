4755d94838f5e4945c6937812da5648dc0642812
Samiksha <samikshadongre12@gmail.com>
2024-05-08 23:02:46 +0530

    2nd file-5May

--- SampleGitTest.java
+++ SampleGitTest.java
@@ -4,6 +4,8 @@ public class SampleGitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to GIT test ");
+		
+		System.out.println("Welcome to GIT test 2  ");
 
 	}
 
---
+++ Test2.java
@@ -0,0 +1,10 @@
+package sampleTest;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("2nd file");
+
+	}
+
+}
